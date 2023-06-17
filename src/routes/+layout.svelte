d98d21b62843d72bc6ad88fb68d84907c2fd375f
James Narayanan <jamesnarayanan@gmail.com>
2023-06-17 18:12:40 -0400

    Added header with title

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<header>
+	<img src="src/assets/buzz.png" alt="Buzz" />
+	<h1>Schedule Sharing</h1>
+</header>
+
+<main>
+	<slot />
+</main>
+
+<style lang="scss">
+	header {
+		display: flex;
+		height: 2.5rem;
+		padding: 0.75rem;
+		border-bottom: 2px solid var(--low-alpha);
+		gap: 0.5rem;
+
+		img {
+			height: 100%;
+		}
+
+		h1 {
+			margin: 0;
+			font-size: 2.5rem;
+			font-weight: 600;
+			line-height: 1;
+		}
+	}
+
+	main {
+		padding: 0.5rem;
+	}
+</style>
