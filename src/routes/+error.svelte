295728c1dc92401c4f7029e78f1686f77ca1337a
James Narayanan <jamesnarayanan@gmail.com>
2023-06-25 17:13:29 -0400

    Added error boundary page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { page } from "$app/stores";
+	import HeaderTitle from "$lib/HeaderTitle.svelte";
+</script>
+
+<HeaderTitle>
+	<a href="/profile" class="button">Profile</a>
+</HeaderTitle>
+<main>
+	<h2>Error {$page.status}</h2>
+	<p>{$page.error?.message}</p>
+</main>
+
+<style lang="scss">
+	.button {
+		align-self: center;
+	}
+
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: 1rem;
+
+		h2 {
+			margin: 0;
+			font-size: 2.5rem;
+		}
+		p {
+			margin: 0;
+		}
+	}
+</style>
