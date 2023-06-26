06e500315bdc96756577186de4c9029319820c60
James Narayanan <jamesnarayanan@gmail.com>
2023-06-26 09:17:58 -0400

    Added table to display my courses

--- src/routes/(main)/profile/+page.server.ts
+++ src/routes/(main)/profile/+page.server.ts
@@ -1,14 +1,22 @@
 import { error } from "@sveltejs/kit";
 
 export async function load({ locals: { supabase } }) {
-	const { data, error: userError } = await supabase.auth.getUser();
-
+	const { data: userData, error: userError } = await supabase.auth.getUser();
 	if (userError) {
 		console.error(userError);
 		throw error(404, "User not found");
-	} else {
-		return {
-			user: data.user
-		};
 	}
+
+	const { data: sectionData, error: sectionError } = await supabase
+		.from("user_sections")
+		.select("sections (name, crn, courses (name), semesters (name))")
+		.eq("user_id", userData.user.id);
+	if (sectionError) {
+		console.error(sectionError);
+		throw error(500, sectionError.message);
+	}
+
+	return {
+		user: { ...userData.user, sections: sectionData }
+	};
 }
--- src/routes/(main)/profile/+page.svelte
+++ src/routes/(main)/profile/+page.svelte
@@ -4,7 +4,32 @@
 	$: user = data.user;
 </script>
 
-<h2>{user.user_metadata.name}'s Courses</h2>
+<h2>My Courses</h2>
+<table>
+	<thead>
+		<tr>
+			<th>Course</th>
+			<th>Section</th>
+			<th>CRN</th>
+			<th>Term</th>
+			<th>Actions</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each user.sections as sectionData}
+			<tr>
+				<td>{sectionData.sections?.courses?.name}</td>
+				<td>{sectionData.sections?.name}</td>
+				<td>{sectionData.sections?.crn}</td>
+				<td>{sectionData.sections?.semesters?.name}</td>
+				<td>
+					<button>Edit</button>
+					<button>Delete</button>
+				</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
 
 <style lang="scss">
 	h2 {
