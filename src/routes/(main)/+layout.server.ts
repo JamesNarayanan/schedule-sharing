e5b0a90859ded372ecd0c3de1e79f2183a2bb52d
James Narayanan <jamesnarayanan@gmail.com>
2023-07-19 23:33:31 -0400

    Added redirect param when redirecting to register

--- src/routes/(main)/+layout.server.ts
+++ src/routes/(main)/+layout.server.ts
@@ -1,8 +1,14 @@
 import { redirect } from "@sveltejs/kit";
 
-export async function load({ locals: { getSession } }) {
+export async function load({ request, locals: { getSession } }) {
 	const session = await getSession();
 	if (!session) {
-		throw redirect(303, "/register");
+		let redirectUrl = "/register";
+		const urlRegex = /\/group\/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$/;
+		const matchText = request.url.match(urlRegex);
+		if (matchText) {
+			redirectUrl += `?redirect=${matchText[0]}`;
+		}
+		throw redirect(303, redirectUrl);
 	}
 }
