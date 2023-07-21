bfa670ce7f7b5c2fef1e3ca4506c148cf17267ae
James Narayanan <jamesnarayanan@gmail.com>
2023-07-20 21:53:18 -0400

    Added morphing plus modal
    Still needs backdrop to stop access to content

--- src/routes/(main)/groupSelect.svelte
+++ src/routes/(main)/groupSelect.svelte
@@ -2,9 +2,14 @@
 	import { goto } from "$app/navigation";
 	import { page } from "$app/stores";
 	import { groupStore } from "$stores/groupStore";
+	import { fade } from "svelte/transition";
 
 	let selectedGroupId: string | null = null;
 	$: groups = $groupStore;
+
+	let modalOpen = false;
+	const transDur = 400;
+	let plus: HTMLDivElement | undefined;
 
 	$: {
 		selectedGroupId = $page.url.pathname.substring(7);
@@ -15,6 +20,27 @@
 		const groupId = select.value;
 		if (groupId) {
 			goto(`/group/${groupId}`);
+		}
+	}
+
+	function toggleModal() {
+		if (!plus) return;
+		plus.style.transitionDuration = `${transDur}ms`;
+		const pos = plus.getBoundingClientRect();
+		if (!modalOpen) {
+			modalOpen = true;
+			plus.style.position = "fixed";
+			plus.style.top = `${pos.y}px`;
+			plus.style.left = `${pos.x}px`;
+		} else {
+			modalOpen = false;
+			setTimeout(() => {
+				if (!plus) return;
+				plus.style.top = "";
+				plus.style.left = "";
+				plus.style.position = "relative";
+				plus.style.transitionDuration = "0s";
+			}, transDur);
 		}
 	}
 </script>
@@ -28,7 +54,26 @@
 			<option value={group.id} selected={group.id === selectedGroupId}>{group.name}</option>
 		{/each}
 	</select>
-	<button class="plus">+</button>
+	<div class="plus" class:modalOpen bind:this={plus}>
+		{#if !modalOpen}
+			<div
+				class="content closed"
+				in:fade={{ delay: transDur / 2, duration: transDur / 2 }}
+				out:fade={{ duration: transDur / 2 }}
+			>
+				<button on:click={toggleModal}>+</button>
+			</div>
+		{:else}
+			<div
+				class="content open"
+				in:fade={{ delay: transDur / 2, duration: transDur / 2 }}
+				out:fade={{ duration: transDur / 2 }}
+			>
+				<button on:click={toggleModal}>&#x2715;</button>
+				<div>Create</div>
+			</div>
+		{/if}
+	</div>
 </div>
 
 <style lang="scss">
@@ -67,12 +112,58 @@
 	}
 
 	.plus {
+		position: relative;
 		border-radius: 50%;
 		height: calc(var(--font-size) * 1.25 + 1rem);
 		width: calc(var(--font-size) * 1.25 + 1rem);
 
-		font-size: calc(var(--font-size) * 1.25);
-		line-height: calc(var(--font-size) * 1.3);
-		padding-bottom: calc(var(--font-size) * 0.1);
+		transition: all ease-in-out;
+		background-color: var(--med-alpha);
+
+		.content {
+			width: 100%;
+			height: 100%;
+			position: absolute;
+
+			display: flex;
+			align-items: center;
+			justify-content: center;
+
+			&.closed button {
+				background-color: transparent;
+				width: 100%;
+				height: 100%;
+				padding: 0;
+				border: none;
+				border-radius: 50%;
+
+				font-size: calc(var(--font-size) * 1.25);
+				line-height: calc(var(--font-size) * 1.3);
+			}
+
+			&.open {
+				button {
+					position: absolute;
+					top: 0.5rem;
+					right: 0.5rem;
+				}
+			}
+		}
+
+		&.modalOpen {
+			position: fixed;
+			top: 50% !important;
+			left: 50% !important;
+			transform: translate(-50%, -50%);
+			width: min(600px, 90%);
+			height: 300px;
+			border-radius: 10px;
+			backdrop-filter: blur(10px);
+			z-index: 999;
+
+			button {
+				outline: none;
+			}
+		}
 	}
 </style>
