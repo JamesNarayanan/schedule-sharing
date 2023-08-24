07e1c3514625528ee3893935bd6401ae4d635504
James Narayanan <jamesnarayanan@gmail.com>
2023-08-24 11:37:36 -0400

    Updated UI
    Now takes up much more space vertically

--- src/routes/(main)/group/[group=uuid]/+page.svelte
+++ src/routes/(main)/group/[group=uuid]/+page.svelte
@@ -19,9 +19,15 @@
 	/**
 	 * Contains the course names for each course number
 	 *
-	 * number: name
+	 * subject + number: name
 	 */
 	const courseNames: Record<string, string> = {};
+	/**
+	 * Contains the subject names for each subject abbreviation
+	 *
+	 * abbreviation: name
+	 */
+	const subjectNames: Record<string, string> = {};
 
 	$: {
 		group = data.group;
@@ -45,6 +51,7 @@
 					continue;
 
 				courseNames[subject + courseNumber] = section.courses?.name || "";
+				subjectNames[subject] = section.courses?.subjects?.name || "";
 
 				if (!tableData[subject]) {
 					tableData[subject] = {};
@@ -71,35 +78,29 @@
 {#if currSemester > 0}
 	<div>
 		{#if Object.keys(tableData).length > 0}
-			<table class="table course-data">
-				<tbody>
-					{#each Object.entries(tableData).sort( ([subA, _A], [subB, _B]) => subA.localeCompare(subB) ) as [subject, courses]}
+			{#each Object.entries(tableData).sort( ([subA, _A], [subB, _B]) => subA.localeCompare(subB) ) as [subject, courses]}
+				<h3 class="subject">{subject} {"\u2013"} {subjectNames[subject]}</h3>
+				<table class="table courses">
+					{#each Object.entries(courses).sort( ([courseNumA, _A], [courseNumB, _B]) => courseNumA.localeCompare(courseNumB) ) as [courseNumber, sections]}
 						<tr>
-							<th>{subject}</th>
-							{#each Object.entries(courses).sort( ([courseNumA, _A], [courseNumB, _B]) => courseNumA.localeCompare(courseNumB) ) as [courseNumber, sections]}
-								<td>
-									<div class="course-details">
-										<h3>{courseNumber}</h3>
-										<span class="tooltip"
-											>{courseNames[subject + courseNumber]}</span
-										>
-									</div>
-									<table class="table section-data">
-										{#each Object.entries(sections).sort( ([secNameA, _A], [secNameB, _B]) => secNameA.localeCompare(secNameB) ) as [sectionName, users]}
-											<tr>
-												<th>{sectionName}</th>
-												{#each users.sort( (userA, userB) => userA.name.localeCompare(userB.name) ) as user}
-													<td>{user.name}</td>
-												{/each}
-											</tr>
-										{/each}
-									</table>
-								</td>
+							<th class="course-details" rowspan="2">
+								<h3>{courseNumber}</h3>
+								<span class="tooltip">{courseNames[subject + courseNumber]}</span>
+							</th>
+							{#each Object.entries(sections).sort( ([secNameA, _A], [secNameB, _B]) => secNameA.localeCompare(secNameB) ) as [sectionName, users]}
+								<th colspan={users.length}>{sectionName}</th>
+							{/each}
+						</tr>
+						<tr>
+							{#each Object.entries(sections).sort( ([secNameA, _A], [secNameB, _B]) => secNameA.localeCompare(secNameB) ) as [_sectionName, users]}
+								{#each users.sort( (userA, userB) => userA.name.localeCompare(userB.name) ) as user}
+									<td>{user.name}</td>
+								{/each}
 							{/each}
 						</tr>
 					{/each}
-				</tbody>
-			</table>
+				</table>
+			{/each}
 		{:else}
 			<h1>No data to display</h1>
 		{/if}
@@ -107,84 +108,47 @@
 {/if}
 
 <style lang="scss">
-	table.course-data {
+	.subject {
+		margin-top: 1.5rem;
+		margin-bottom: 0.25rem;
+		font-size: 2rem;
+	}
+	table.courses {
 		text-align: center;
 
-		td {
-			padding: 0;
+		.course-details {
+			position: relative;
 
-			.course-details {
-				position: relative;
+			.tooltip {
+				--padding-top: 0.75rem;
+				position: absolute;
+				max-width: 150px;
+				top: 60px;
+				padding: 0.5rem var(--padding-top);
+				border-radius: 0.5rem;
+				transform: translateX(-50%);
+				font-size: 0.8rem;
+				color: var(--text);
+				background-color: var(--bg);
+				z-index: 2;
 
-				h3 {
-					margin: 0;
-					padding: 0 0.5rem;
-					border-bottom: 2px solid var(--med-alpha);
-				}
-				.tooltip {
-					--padding-top: 0.75rem;
+				opacity: 0;
+				transition: opacity 0.2s;
+				pointer-events: none;
+
+				&::before {
+					content: "";
 					position: absolute;
-					max-width: 150px;
-					top: 26px;
-					padding: 0.5rem var(--padding-top);
-					border-radius: 0.5rem;
-					transform: translateX(-50%);
-					font-size: 0.8rem;
-					color: var(--text);
+					left: 50%;
+					transform: translate(-50%, calc(-1 * var(--padding-top) + 2px)) rotate(45deg);
 					background-color: var(--bg);
-					z-index: 2;
-
-					opacity: 0;
-					transition: opacity 0.2s;
-					pointer-events: none;
-
-					&::before {
-						content: "";
-						position: absolute;
-						left: 50%;
-						transform: translate(-50%, calc(-1 * var(--padding-top) + 2px))
-							rotate(45deg);
-						background-color: var(--bg);
-						padding: 4px;
-						z-index: 1;
-					}
-				}
-
-				h3:hover + .tooltip {
-					opacity: 1;
+					padding: 4px;
+					z-index: 1;
 				}
 			}
 
-			table.section-data {
-				width: 100%;
-				border-radius: 0;
-				background-color: transparent;
-
-				tr {
-					display: table-cell;
-
-					&:not(:last-child) {
-						border-right: 2px solid var(--med-alpha);
-					}
-
-					td,
-					th {
-						display: block;
-
-						padding: 0.25rem 0.5rem;
-						border: none;
-						border-radius: 0;
-						background: none;
-					}
-
-					th {
-						border-bottom: 2px solid var(--med-alpha);
-					}
-
-					td:not(:last-child) {
-						border-bottom: 1px solid var(--med-alpha);
-					}
-				}
+			h3:hover + .tooltip {
+				opacity: 1;
 			}
 		}
 	}
